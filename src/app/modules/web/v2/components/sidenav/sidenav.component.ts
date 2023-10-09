e86529505da8a851467b191c1dd897e80d526244
diegohyenna <dgmartins87@hotmail.com>
2023-10-09 17:25:41 -0300

    finalizado header, editando page hello, component sidenav

--- src/app/modules/web/v2/components/sidenav/sidenav.component.ts
+++ src/app/modules/web/v2/components/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'v2-app-sidenav',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.scss'],
 })
 export class V2SidenavComponent implements OnInit {
+  @Input() showLeft = true;
+
   constructor() {}
 
   ngOnInit(): void {}
